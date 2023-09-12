4d3c5322ded032e453e2b55bff5d40b48791ddde
Vladulka <Vladikkus1628105123698745>
2023-09-12 15:07:16 +0300

    Итоговый результат шестого спринта #2

--- cypress/e2e/dnd-constructor.cy.js
+++ cypress/e2e/dnd-constructor.cy.js
@@ -2,7 +2,6 @@ describe("Checking dnd - constructor and own pages for feeds and ingredients", f
     beforeEach(() => {
         cy.visit("http://localhost:3000");
         cy.intercept("GET", "api/all-ingredients", {fixture: "ingredients.json"});
-        cy.intercept("GET", "api/auth", {fixture: "auth.json"});
     });
 
     const burgerConstructor = '[data-test-market="burger-constructor"]';
@@ -12,39 +11,6 @@ describe("Checking dnd - constructor and own pages for feeds and ingredients", f
         cy.get(currentIngredient).each((element) => {
             cy.wrap(element).trigger("dragstart");
             cy.get(burgerConstructor).trigger("drop");
-        });
-    });
-
-    it("Check creating order with ingredients when user be sing in", () => {
-        cy.get('[data-test-marker="bun"]').find(currentIngredient).first().trigger("dragstart");
-        cy.get(burgerConstructor).trigger("drop");
-
-        cy.get('[data-test-marker="main"]').find(currentIngredient).each((element) => {
-            cy.wrap(element).trigger("dragstart");
-            cy.get(burgerConstructor).trigger("drop");
-        });
-
-        cy.get('[data-test-marker="sauce"]').find(currentIngredient).each((element) => {
-            cy.wrap(element).trigger("dragstart");
-            cy.get(burgerConstructor).trigger("drop");
-        });
-
-        cy.get('[data-test-marker="submit-order-button"]').click();
-    });
-
-    it("Check correct opening and closing ingredient details modal", () => {
-
-        const regEx = 'not.have.text';
-        cy.get(currentIngredient).each((element) => {
-            cy.wrap(element).click();
-
-            cy.get('[data-test-marker="name"]').should(regEx, '');
-            cy.get('[data-test-marker="calories"]').should(regEx, '');
-            cy.get('[data-test-marker="proteins"]').should(regEx, '');
-            cy.get('[data-test-marker="fat"]').should(regEx, '');
-            cy.get('[data-test-marker="carbohydrates"]').should(regEx, '');
-
-            cy.get('[data-test-marker="modal-close"]').click();
         });
     });
 });
--- src/components/burger-constructor/order-details/order-details.tsx
+++ src/components/burger-constructor/order-details/order-details.tsx
@@ -5,11 +5,11 @@ import { useAppSelector } from "../../../utils/hooks";
 
 const OrderDetails = () => {
 
-    const orderData = useAppSelector((store) => store.orderDetails);
+    const orderData = useAppSelector((store) => store.orderDetails.order);
 
     return (
         <div className={style.order_details}>
-            <p className={`text text_type_digits-large mt-30 ${style.order_num}`}>{orderData.order.number}</p>
+            <p className={`text text_type_digits-large mt-30 ${style.order_num}`} data-test-marker="order-number">{orderData.number}</p>
             <p className="text text_type_main-medium mt-8 mb-15">идентификатор заказа</p>
             <Done />
             <p className="text text_type_main-default mt-15">Ваш заказ начали готовить</p>
